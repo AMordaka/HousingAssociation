51d117f4e34d528fe10bb0d4825780d274af9547
Arkadiusz Mordaka <arkadiusz1057@gmail.com>
2019-03-30 12:49:52 +0100

    Stworzenie warstwy Service

---
+++ src/main/java/pl/dmcs/mordaka/arkadiusz/app/service/UserService.java
@@ -0,0 +1,10 @@
+package pl.dmcs.mordaka.arkadiusz.app.service;
+
+import pl.dmcs.mordaka.arkadiusz.app.model.User;
+
+public interface UserService {
+
+    User findByLogin(String login);
+
+    void registerUser(User user);
+}
