ea09160714b77a29a24132b11872df24e50b83ca
Arkadiusz Mordaka <arkadiusz1057@gmail.com>
2019-04-07 11:07:37 +0200

    [BUG] Przypisywanie roli do użytkownika

--- src/main/java/pl/dmcs/mordaka/arkadiusz/app/model/Role.java
+++ src/main/java/pl/dmcs/mordaka/arkadiusz/app/model/Role.java
@@ -4,17 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
 
 @Data
 @Entity
 @Table(name = "ROLE_T")
 @AllArgsConstructor
 @NoArgsConstructor
-public class Role extends BaseEntity {
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 
     @NotEmpty
     @Column(name = "ROLE", unique = true, nullable = false)
